5d0822533014f6c971cf3282fda1e4a14c4c1b9b
slowdolphin <tighe59@gmail.com>
2021-06-30 20:54:39 -0400

    show page css is done-ish

--- views/Show.jsx
+++ views/Show.jsx
@@ -8,13 +8,17 @@ class Show extends React.Component{
       <DefaultLayout styles={[{key:0, href:'/css/showpage.css'}]}>
         <h1>Show Page</h1>
         <div className = "container">
-          <div> Name: {product.name} </div>
-          <div> Price: {product.price} Gold </div>
-          <div> Quantity:{product.qty} </div>
-          <div>Description: {product.description}</div>
-        <img src={product.img}/>
-        <nav>
-          <a href="/product/">Home</a>
+          <div className = "productName">{product.name} </div>
+          <div className = "numbBoi">
+            <div className = "productPrice"> Price: {product.price} Gold </div>
+            <div className = "qty"> Available:{product.qty} </div>
+            <div className ="buy">BUY</div>
+            <img className = "pic" src={product.img}/>
+          </div>
+          <div className = "scrip">Description: {product.description}</div>
+        <nav className = "Nav">
+          <a  className = "home" href="/product/">Home</a> <br/>
+          <a className = "contact" href = "/product/contact">Contact Info</a>
         </nav>
         </div>
       </DefaultLayout>
